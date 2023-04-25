875e102131a3cba1b68adc465012cb12da4f0d46
Kevin Lam <Kevin@Administrators-MacBook-Pro.local>
2023-04-25 11:10:01 -0400

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+// In this challenge, you’ll have a function that takes an array as input and returns an array. But if all goes according to plan, it’ll remove the negative numbers. This is another example of a task that’ll be useful when combing through data and looking for clever ways to eliminate “bad data.”
+function array(x, y, z) {
+  // array checks if each argument is a positive number using our typeOf keyword
+  if ((typeof x, y, z >= 10000)) {
+    console.log("Yep. They are all positive numbers ");
+  } else {
+  }
+}
+
+array();
